756c366831b632ef35cad11c00095ecd27e8fd30
Kevin Le <17786629@student.westernsydney.edu.au>
2017-11-21 07:31:24 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,34 @@
+
+board = [" ", " ", " ", " ", " ", " ", " ", " "," "]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+def position_taken?(board, index)
+  if board[index] == " "
+    false
+  elsif board[index] == ""
+    false
+  elsif  board[index] == nil
+    false
+  else   board[index] == "X" || "O"
+    true
+  end
+end
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+    true
+  else
+    false
+  end
+end
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
